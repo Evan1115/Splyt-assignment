ca0be182a524f6bcdd30012277f66859a8bf2a0e
Joram <jing111597@gmail.com>
2021-06-21 07:26:42 +0800

    minor modification

--- task1.js
+++ task1.js
@@ -34,18 +34,18 @@ function defaultArguments(operator, value) {
 }
 const add2 = defaultArguments(add, { b: 9 })
 
-console.log(add2(10) === 19)
-console.log(add2(10, 7) === 17)
-console.log(isNaN(add2()));
+console.assert(add2(10) === 19)
+console.assert(add2(10, 7) === 17)
+console.assert(isNaN(add2()));
 
 const add3 = defaultArguments(add2, { b: 3, a: 2 });
-console.log(add3(10) === 13);
-console.log(add3() === 5);  // 3 + 2 = 5  - both value zero 
-console.log(add3(undefined, 10) === 12);
+console.assert(add3(10) === 13);
+console.assert(add3() === 5);  // 3 + 2 = 5  - both value zero 
+console.assert(add3(undefined, 10) === 12);
 
 const add4 = defaultArguments(add, { c: 3 }); // doesn't do anything, since c isn't an argument
-console.log(isNaN(add4(10)));
-console.log(add4(10, 10) === 20);
-console.log("")
+console.assert(isNaN(add4(10)));
+console.assert(add4(10, 10) === 20);
+
 
 
